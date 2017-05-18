af944f77e02437445cddefaec7b4152edca6c9be
sherlyc <sherlyc@gmail.com>
2017-05-18 15:28:10 +1200

    knex setup

---
+++ knexfile.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    },
+    useNullAsDefault: true
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL || {}
+  },
+
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: ':memory'
+    },
+    seeds: {
+      directory: './tests/helpers/seeds'
+    }
+  }
+};
--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 var server = require('./server')
 
+var environment = process.env.NODE_ENV || 'development'
+var config = require('../knexfile')[environment]
+var connection = require('knex')(config)
+
 var PORT = process.env.PORT || 3000
 
 server.listen(PORT, function () {
   console.log('Listening on port', PORT)
 })
-
