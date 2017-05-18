bfa1e822d9bd6a0b17535be34b4fc080a83bc2d2
matt-raines <mattraines.nz@gmail.com>
2017-05-18 14:00:46 +1200

    setup of react and first home route done

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import List from './List'
+import {HashRouter as Router, Route} from 'react-router-dom'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <Router>
+      <div>
+        <h1>Welcome to Schoolz</h1>
+        <hr></hr>
+          <div className='container'>
+            <div className='nav'>
+              Navigation menu goes here
+            </div>
+            <hr></hr>
+            <div className='content'>
+              <Route exact={true} path='/' component={List}/>
+            </div>
+          </div>
+      </div>
+    </Router>
+
   )
 }
 
 export default App
-
---
+++ client/components/List.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const List = props => {
+
+  return (
+    <div>
+      <p>Blah blah blah, consectetur adipiscing elit. Quisque mattis nisl leo, quis fermentum metus suscipit ut. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a suscipit nisl. Etiam posuere, massa a rutrum pellentesque, urna nisi dapibus sapien, a malesuada metus lorem eget mauris. Nullam gravida sapien fermentum lorem sollicitudin sagittis ac id mi. Donec iaculis sem nec blandit tempus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec a urna in ex tempor ullamcorper vel ut metus. Praesent pulvinar tincidunt urna et hendrerit. Aenean volutpat eleifend scelerisque. Cras vestibulum vel felis luctus accumsan.</p>
+      <p>In bibendum, dolor quis convallis lacinia, arcu turpis accumsan dolor, at accumsan nibh sapien non turpis. Etiam semper ac eros nec vulputate. Suspendisse potenti. Phasellus vitae dolor ac nisl ultricies efficitur. Mauris blandit quis arcu in ultrices. Proin tristique purus ut libero placerat luctus a at risus. In sodales nisi nec urna sagittis, et vestibulum est lobortis. Donec ut quam at quam sagittis tempus bibendum eget turpis. Fusce neque leo, aliquet nec feugiat ut, finibus at ante. Vestibulum at mauris placerat, sollicitudin augue eget, consequat lacus. Aliquam tristique convallis odio, et cursus lectus pellentesque eget. Vivamus id lorem sed sem ullamcorper volutpat et eu ligula. Maecenas tristique purus id ligula lacinia, eget convallis leo gravida. Suspendisse sem elit, hendrerit eu sapien vel, molestie pellentesque neque. Pellentesque dictum tincidunt turpis in hendrerit.</p>
+    </div>
+  )
+}
+
+export default List
