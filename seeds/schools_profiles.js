e4f9e6217c19a7ad0c122d4751f9dd2d8fc1622e
Sherly Chan <sherlyc@gmail.com>
2017-05-25 11:33:01 +1200

    added locations seed

--- seeds/schools_profiles.js
+++ seeds/schools_profiles.js
@@ -11,7 +11,7 @@ exports.seed = function(knex, Promise) {
                 knex('schools').insert({name: "Wellington Girl's College", schoolType: 'Secondary (Year 9-15)', authority: 'State', gender: 'Girls School', decile: '10'}),
                 knex('schools').insert({name: "St Patrick's College", schoolType: 'Secondary (Year 9-15)', authority: 'State', gender: 'Boys School', decile: '8'}),
                 knex('schools').insert({name: "Kelburn Normal School", schoolType: 'Full Primary School (Year 1-8)', authority: 'State', gender: 'Co-Educational', decile: '10'}),
-                knex('schools').insert({name: "Adventure", schoolType: 'Full Primary School (Year 1-8)', authority: 'State', gender: 'Co-Educational', decile: '10'}),
+                knex('schools').insert({name: "Adventure School", schoolType: 'Full Primary School (Year 1-8)', authority: 'State', gender: 'Co-Educational', decile: '10'}),
                 knex('profiles').insert({address: "Pipitea Street, Thorndon", email: "ann.gilbert@wgc.school.nz", url: "http://www.wgc.school.nz", school_id:'1'}),
                 knex('profiles').insert({address: "581 Evans Bay Parade, Kilbirnie", email: "admin@stpats.school.nz", url: "http://www.stpats.school.nz/", school_id:'2'}),
                 knex('profiles').insert({address: "Kowhai Road, Kelburn", email: "admin@kelburnnormal.school.nz", url: "http://www.kelburnnormal.school.nz/", school_id:'3'}),
