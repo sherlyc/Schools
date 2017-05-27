c9226b6dc947237a6c9a81cb7a1cafc73bdb4dec
Sherly Chan <sherlyc@gmail.com>
2017-05-27 20:27:33 +1200

    changed seeeds data

--- seeds/schools_profiles.js
+++ seeds/schools_profiles.js
@@ -20,8 +20,8 @@ exports.seed = function(knex, Promise) {
                 knex('profiles').insert({address: "581 Evans Bay Parade, Kilbirnie", email: "admin@stpats.school.nz", url: "http://www.stpats.school.nz/", school_id:'2'}),
                 knex('profiles').insert({address: "Kowhai Road, Kelburn", email: "admin@kelburnnormal.school.nz", url: "http://www.kelburnnormal.school.nz/", school_id:'3'}),
                 knex('profiles').insert({address: "Longitude Place, Porirua", email: "office@adventure.school.nz", url: "http://www.adventure.school.nz", school_id:'4'}),
-                knex('locations').insert({latitude: "-41.275422", longitude: "174.780672", suburb: "Thorndon", school_id:'1'}),
-                knex('locations').insert({latitude: "-41.313832", longitude: "174.795985", suburb: "Kilbirnie", school_id:'2'}),
+                knex('locations').insert({latitude: "-41.313832", longitude: "174.795985", suburb: "Kilbirnie", school_id:'1'}),
+                knex('locations').insert({latitude: "-41.275422", longitude: "174.780672", suburb: "Thorndon", school_id:'2'}),
                 knex('locations').insert({latitude: "-41.287277", longitude: "174.761649", suburb: "Kelburn", school_id:'3'}),
                 knex('locations').insert({latitude: "-41.120037", longitude: "174.890083", suburb: "Whitby", school_id:'4'})
 
